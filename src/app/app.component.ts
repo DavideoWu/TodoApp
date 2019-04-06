ca97e8ba985f07d8b9f8fb61d7f744969345b442
David Wu <you@example.com>
2019-04-06 11:44:37 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Todo-App';
+  todoItems = ["laundry", "homework", "eat"];
+
+  deleteTodo(todo) {
+    this.todoItems = this.todoItems.filter((item) => {
+      return item !== todo;
+    });
+  }
+
+  todoSubmit(value: any) {
+    if (value !== "") {
+      this.todoItems.push(value.todo);
+    } else {
+      alert('Field requied **')
+    }
+  }
 }
